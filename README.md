7c0efd8aaf12cfe841d5485d8ff6446349bfdfaf
harshalyaravalkar <harshalyaravalkar492@gmail.com>
2023-10-07 01:29:18 +0530

    wrote readme file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Python_File-handling
 This Repository contains a file handling in Python Project
+<br><br>
+Overview
+
+This Python project demonstrates various file handling operations, including creating, reading, rewriting, and appending to text files. This project provides practical examples of working with files in Python.
+
+## Features
+
+- **Text File Handling:**
+  - **Creating Files:** Demonstrates how to create a new text file.
+  - **Reading Files:** Shows how to read the contents of a text file.
+  - **Rewriting Files:** Illustrates how to modify and rewrite the content of a text file.
+  - **Appending to Files:** Explains how to add new content to an existing text file using append mode.
+
+repository contains two files other than main python code and this readme file that are sample.txt file and data.txt file. sample file is just a file with some text lines and data file contains a strctured data in text file. They can be used to demonstrate python code.
--- file_handling.py
+++ file_handling.py
@@ -1,27 +1,27 @@
-def creatf(name):
+def creatf(name): # This function creates a empty text file
     file = open(f"{name}.txt","a")
     file.close()
 
 
-def readf(name):
+def readf(name): # This function reads and returns content of text file
     file = open(f"{name}.txt","r")
     content = file.read()
     file.close()
     return content
 
-def writef(text,filename):
+def writef(text,filename): # This function rewrites file with user input text
     file = open(f"{filename}.txt","w")
     charlen = file.write(text)
     print(f"You have successfully written in file, Total Character Length: {charlen}")
     file.close()
 
-def addf(text, filename):
+def addf(text, filename): # This function adds the text without rewriting the file 
     file = open(f"{filename}.txt","a")
     charlen = file.write(f"\n{text}")
     print(f"You have successfully written in file, Total Character length: {charlen}")
     file.close()
 
-def database(data):
+def database(data): # This function adds records in database file
     file = open("database.txt","a")
     for i,n,a,l in data:
         file.write(f"{i:5} {n:15} {a:3} {l:12}\n")
@@ -34,46 +34,46 @@ while True:
     print("To Work with: \n 1. Create a Text File \n 2. Text File \n 3. Structured data file \n 4. Exit\n")
     s = input("Enter your selection: ")
 
-    if(s=="1"):
+    if(s=="1"): #Create text file
          name = input("Enter name of file: ")
          creatf(name)
          continue
 
-    if(s=="2"):
+    if(s=="2"): # Actions on text file
         print("\nText File: \n 1. Read text File \n 2. Overwrite text file \n 3. Add text into file \n 4. Main menu \n")
         n = input("Enter your selection: ")
 
-        if(n=="1"):
+        if(n=="1"): # Reading text file
             name = input("Enter name of file: ")
             content = readf(name)
             print(f"\n\n{content}\n")
             continue
 
-        if(n=="2"):
+        if(n=="2"): # Rewriting content in file
             text = input("Enter the text content you want to add: ")
             filename = input("Enter the filename: ")
             writef(text,filename)
             continue
 
-        if(n=="3"):
-            text = input("Enter the text you want toadd in existing content of file: ")
+        if(n=="3"): # Adding content in file
+            text = input("Enter the text you want to add in existing content of file: ")
             filename = input("Enter the file name: ")
             addf(text,filename)
             continue
         else:
             continue
 
-    if(s=="3"):
+    if(s=="3"): # Actions on database file
         print("\nStructured Data: \n 1. Read Structured Data File \n 2. Add Records \n 3. Exit\n")
         r = input("Enter your selection: ")
 
-        if(r=="1"):
+        if(r=="1"): # Read database file
             name = input("Enter name of file: ")
             content = readf(name)
             print(f"\n\n{content}\n")
             continue
 
-        if(r=="2"):
+        if(r=="2"): # Add records in database file
             print("Enter data in the file: ")
             ids = input("Enter the id: ")
             name = input("Enter the name: ")
@@ -85,7 +85,7 @@ while True:
             continue
         
         else:
-            continue
+            continue # Takes back to main menu
 
     else:
-        break
+        break # Ends program
